33df69863d90d23e1e7794a4e3720562a23f2f9d
Mike Dent <mcdent@gmail.com>
2023-04-26 14:03:20 +0100

    fleek: apply

--- user.nix
+++ user.nix
@@ -2,7 +2,7 @@
   # This file will never be modified by fleek
   # configs mentioned here must be listed in ~/fleek.yml #programs array or you will get errors
 shellOptions = [
-"noclobber"
-];
+	"noclobber"
+	];
  
 }
