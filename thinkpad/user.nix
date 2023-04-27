dc0cea8bece7a4063ccdd7a31af3eda395883e72
mcdent <mcdent@gmail.com>
2023-04-27 21:03:26 +0100

    fleek: apply

--- thinkpad/user.nix
+++ thinkpad/user.nix
@@ -5,4 +5,22 @@ programs.librewolf.enable = true;
 # The following 2 lines set XDG paths correctly for non nix hosts. 
 targets.genericLinux.enable = true;
 xdg.enable = true;
+
+programs.starship.settings = {
+  add_newline = false;
+  format = lib.concatStrings [
+    "$line_break"
+    "$package"
+    "$line_break"
+    "$character"
+  ];
+  scan_timeout = 10;
+  character = {
+    success_symbol = "➜";
+    error_symbol = "➜";
+  };
+}
+
+
+
 }
