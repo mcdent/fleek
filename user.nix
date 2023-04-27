2d7af1df9640b921ec3deef53fe3758306214813
mcdent <mcdent@gmail.com>
2023-04-27 09:45:00 +0100

    fleek: apply

--- user.nix
+++ user.nix
@@ -1,9 +1,9 @@
 { pkgs, misc, ... }: {
   # This file will never be modified by fleek
   # configs mentioned here must be listed in ~/fleek.yml #programs array or you will get errors
-home.sessionVariables = {
-  XDG_CONFIG_HOME = "~/.config";
-};
+#home.sessionVariables = {
+#  XDG_CONFIG_HOME = "~/.config";
+#};
 programs.bash =	{
 	enable = true;
 initExtra = 
