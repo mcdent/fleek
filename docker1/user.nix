7721d4e23276808795063bf3c7fefc3f04ea0af0
mcdent <mcdent@gmail.com>
2023-04-27 17:54:19 +0100

    fleek: apply

--- docker1/user.nix
+++ docker1/user.nix
@@ -1,6 +1,8 @@
 { pkgs, misc, ... }: {
   # This file will never be modified by fleek
   # configs mentioned here must be listed in ~/fleek.yml #programs array or you will get errors
-
+# The following 2 lines set XDG paths correctly for non nix hosts. 
+targets.genericLinux.enable = true;
+xdg.enable = true;
  
 }
