f59e8864401808d65de124937947720e1f3ba39b
Mike Dent <mcdent@gmail.com>
2023-04-27 23:19:11 +0100

    fleek: apply

--- user.nix
+++ user.nix
@@ -11,4 +11,19 @@ initExtra =
 	;
 };
 
+programs.starship.settings = {
+  add_newline = false;
+  format = lib.concatStrings [
+    "$line_break"
+    "$package"
+    "$line_break"
+    "$character"
+  ];
+  scan_timeout = 10;
+  character = {
+    success_symbol = "➜";
+    error_symbol = "➜";
+  };
+}
+
 }
