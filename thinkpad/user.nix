0bba55334a6099253d5319e3c5c0bd18d17b2b27
mcdent <mcdent@gmail.com>
2023-04-27 21:01:12 +0100

    fleek: apply

--- thinkpad/user.nix
+++ thinkpad/user.nix
@@ -2,7 +2,6 @@
   # This file will never be modified by fleek
   # configs mentioned here must be listed in ~/fleek.yml #programs array or you will get errors
 programs.librewolf.enable = true;
-programs.librewolf.package = pkgs.librewolf-wayland;
 # The following 2 lines set XDG paths correctly for non nix hosts. 
 targets.genericLinux.enable = true;
 xdg.enable = true;
