983e3564b333a4e76a4e65da2660ed02d34f3585
Mike Dent <mcdent@gmail.com>
2023-05-04 16:27:55 +0100

    fleek: apply

--- user.nix
+++ user.nix
@@ -14,7 +14,7 @@ initExtra =
       enable = true;
       settings = {
         # "$schema" = "https://starship.rs/config-schema.json";
-        add_newline = true;
+        add_newline = false;
         command_timeout = 500;
         continuation_prompt = "[∙](bright-black) ";
         format = "[](0x9A348E)$username$hostname$localip$shlvl$singularity$kubernetes[](fg:0x9A348E bg:0xDA627D)$directory$vcsh[](fg:0xDA627D bg:0xFCA17D)$git_branch$git_commit$git_state$git_metrics$git_status$hg_branch[](fg:0x86BBD8 bg:0x06969A)$docker_context$package$buf[](fg:0xFCA17D bg:0x86BBD8)$c$cmake$cobol$container$daml$dart$deno$dotnet$elixir$elm$erlang$golang$haskell$helm$java$julia$kotlin$lua$nim$nodejs$ocaml$perl$php$pulumi$purescript$python$rlang$red$ruby$rust$scala$swift$terraform$vlang$vagrant$zig$nix_shell$conda$spack$memory_usage$aws$gcloud$openstack$azure$env_var$crystal$custom$sudo$cmd_duration$line_break$jobs$battery[](fg:0x06969A bg:0x33658A)$time$status$shell$character";
@@ -900,9 +900,7 @@ initExtra =
         };
         custom = {
         };
-        add_newline = true;
       };
-        add_newline = true;
     };
     
 ## Above is Starship config
